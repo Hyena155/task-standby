883810283c32d384b0b6fe273ffd320b37356ffa
Takahiro KISHI <kojika@Hyena-Macbook.local>
2026-05-01 14:49:42 +0900

    fix: Service Workerキャッシュを強制更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'task-standby-v1';
+const CACHE = 'task-standby-v3';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
